6ff537a0e4dec3808ba49c6bfc881dbaa7dae999
Kat <thekatlabkittens@gmail.com>
2018-10-25 20:39:19 +0200

    FCC Technical documentation page
    Freecodecamp assignment

---
+++ TechnicalDocumentationPage/show_text.js
@@ -0,0 +1,10 @@
+
+// $(document).ready(function() {
+
+function showText(a) {
+    var headers = ["INTRO", "OVERVIEW", "BEGINNERS", "INTERMEDIATE", "ADVANCED"]
+    var header = headers[a];
+
+    document.getElementById("text-link").innerHTML = header;
+
+}
